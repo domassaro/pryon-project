b0ddf199dc77295beca2ba4ecad2327ad3092def
Deirdre <deirdremassaro@Deirdres-MacBook-Pro.local>
2023-09-10 18:22:10 -0700

    Update navigation bar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,92 +1,29 @@
-import LoginButton from "../components/login-button";
-import {
-  Card,
-  CardBody,
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Link,
-  Button,
-  User,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownTrigger,
-} from "@nextui-org/react";
 import React from "react";
-import { useRouter } from "next/router";
-import { useSession, signOut, signIn } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
+import { Card, CardBody, Link, Button } from "@nextui-org/react";
+import { Navigation } from "./navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
   const { data: session } = useSession();
 
   if (session) {
     return (
       <div className="min-h-screen h-14 bg-gradient-to-r from-purple-500 to-pink-500">
-        <Navbar>
-          <NavbarBrand>
-            <p className="font-bold text-inherit">Pryon</p>
-          </NavbarBrand>
-          <NavbarContent className="hidden sm:flex gap-4" justify="center">
-            <NavbarItem isActive={router.asPath.includes("profile")}>
-              <Link href="/profile" color="foreground">
-                Profile
-              </Link>
-            </NavbarItem>
-            <NavbarItem isActive={router.asPath.includes("astronauts")}>
-              <Link href="/astronauts" color="foreground">
-                Astronauts
-              </Link>
-            </NavbarItem>
-            <NavbarItem isActive={router.asPath.includes("location")}>
-              <Link color="foreground" href="/location">
-                ISS Location
-              </Link>
-            </NavbarItem>
-          </NavbarContent>
-          <NavbarContent justify="end">
-            <NavbarItem className="hidden lg:flex">
-              <Dropdown>
-                <DropdownTrigger>
-                  <User
-                    name={session.user?.name}
-                    description={session.user?.email}
-                    avatarProps={{
-                      src: session.user?.image,
-                    }}
-                  />
-                </DropdownTrigger>
-                <DropdownMenu aria-label="Static Actions">
-                  <DropdownItem
-                    key="delete"
-                    className="text-danger"
-                    color="danger"
-                    onClick={() => signOut()}
-                  >
-                    Logout
-                  </DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-            </NavbarItem>
-          </NavbarContent>
-        </Navbar>
-        <LoginButton />
+        <Navigation session={session} />
         <div className="flex flex-col items-center justify-center py-8 px-2">
-          hello
           {children}
         </div>
       </div>
     );
   }
+
   return (
     <>
       <div className="min-h-screen h-14 bg-gradient-to-r from-purple-500 to-pink-500 flex flex-col items-center justify-center py-8">
         <Card>
           <CardBody>
             <Button
-              onClick={() => signIn()}
+              onClick={() => signIn("pryon", { callbackUrl: "/profile" })}
               as={Link}
               color="default"
               href="#"
---
+++ src/components/navigation.tsx
@@ -0,0 +1,75 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  User,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react";
+import { useRouter } from "next/router";
+import { signOut } from "next-auth/react";
+import { Session } from "next-auth";
+
+interface Props {
+  session: Session;
+}
+
+export const Navigation = ({ session }: Props): JSX.Element => {
+  const router = useRouter();
+
+  return (
+    <>
+      <Navbar>
+        <NavbarBrand>
+          <p className="font-bold text-inherit">Pryon</p>
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem isActive={router.asPath.includes("profile")}>
+            <Link href="/profile" color="foreground">
+              Profile
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive={router.asPath.includes("astronauts")}>
+            <Link href="/astronauts" color="foreground">
+              Astronauts
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive={router.asPath.includes("location")}>
+            <Link color="foreground" href="/location">
+              ISS Location
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem className="hidden lg:flex">
+            <Dropdown>
+              <DropdownTrigger>
+                <User
+                  name={session.user?.name}
+                  description={session.user?.email}
+                  avatarProps={{
+                    src: session.user?.image,
+                  }}
+                />
+              </DropdownTrigger>
+              <DropdownMenu aria-label="Static Actions">
+                <DropdownItem
+                  key="delete"
+                  className="text-danger"
+                  color="danger"
+                  onClick={() => signOut()}
+                >
+                  Logout
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    </>
+  );
+};
