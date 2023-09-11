550491f7c7080a999511beffd6e84cf7c3c0c860
Deirdre <deirdremassaro@Deirdres-MacBook-Pro.local>
2023-09-11 16:34:24 -0700

    Clean up

--- src/tests/astronauts.test.tsx
+++ src/tests/astronauts.test.tsx
@@ -1,45 +1,45 @@
-// import React from "react";
-// import { render, screen } from "@testing-library/react";
-// import Astronauts from "../pages/astronauts";
-// import { useSession } from "next-auth/react";
-// import API from "../pages/api/astronauts";
-// import "@testing-library/jest-dom";
-// jest.mock("next-auth/react");
-// jest.mock("../pages/api/astronauts");
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Astronauts from "../pages/astronauts";
+import { useSession } from "next-auth/react";
+import API from "../pages/api/astronauts";
+import "@testing-library/jest-dom";
+jest.mock("next-auth/react");
+jest.mock("../pages/api/astronauts");
 
-// describe("Astronauts", () => {
-//   it("Astronauts page loads", async () => {
-//     const mockSession = {
-//       expires: "1",
-//       user: { email: "deirdre.massaro@gmail.com", name: "Deirdre", image: "c" },
-//     };
+describe("Astronauts", () => {
+  it("Astronauts page loads", async () => {
+    const mockSession = {
+      expires: "1",
+      user: { email: "deirdre.massaro@gmail.com", name: "Deirdre", image: "c" },
+    };
 
-//     (useSession as jest.Mock).mockReturnValueOnce([mockSession, false]);
+    (useSession as jest.Mock).mockReturnValueOnce([mockSession, false]);
 
-//     const mockResponse = {
-//       message: "success",
-//       number: 1,
-//       people: [
-//         {
-//           craft: "ISS",
-//           name: "Sergey Prokopyev",
-//         },
-//       ],
-//     };
+    const mockResponse = {
+      message: "success",
+      number: 1,
+      people: [
+        {
+          craft: "ISS",
+          name: "Sergey Prokopyev",
+        },
+      ],
+    };
 
-//     jest.fn().mockResolvedValue(mockResponse);
-//     const mockFetch = jest.fn().mockImplementation(() => ({
-//       json: jest.fn().mockResolvedValue(mockResponse),
-//     }));
+    jest.fn().mockResolvedValue(mockResponse);
+    const mockFetch = jest.fn().mockImplementation(() => ({
+      json: jest.fn().mockResolvedValue(mockResponse),
+    }));
 
-//     jest.mock("node-fetch", () => {
-//       return jest.fn().mockImplementation((...p) => {
-//         return mockFetch(...p);
-//       });
-//     });
+    jest.mock("node-fetch", () => {
+      return jest.fn().mockImplementation((...p) => {
+        return mockFetch(...p);
+      });
+    });
 
-//     render(<Astronauts />);
+    render(<Astronauts />);
 
-//     expect(screen.getByTestId("astronaut-wrapper")).toBeInTheDocument();
-//   });
-// });
+    expect(screen.getByTestId("astronaut-wrapper")).toBeInTheDocument();
+  });
+});
