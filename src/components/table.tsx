13915d475fad04722e31b8cc8e66c3d162cdcfd8
Deirdre <deirdremassaro@Deirdres-MacBook-Pro.local>
2023-09-11 18:03:43 -0700

    Add search bar

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -9,8 +9,9 @@ import {
   TableRow,
   TableCell,
   Spinner,
+  Input,
 } from "@nextui-org/react";
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 interface Props {
   astronauts: Astronaut[];
@@ -18,51 +19,91 @@ interface Props {
 
 export const TableWrapper = ({ astronauts }: Props): JSX.Element => {
   const [page, setPage] = useState<number>(1);
+  const [filterValue, setFilterValue] = useState<string>("");
+  const hasSearchFilter = Boolean(filterValue);
+
   const rowsPerPage = 5;
-  const pages = Math.ceil(astronauts.length / rowsPerPage);
+
+  const onSearchChange = useCallback((value: string) => {
+    if (value) {
+      setFilterValue(value);
+      setPage(1);
+    } else {
+      setFilterValue("");
+    }
+  }, []);
+
+  const filteredItems = useMemo(() => {
+    let filteredUsers = [...astronauts];
+
+    if (hasSearchFilter) {
+      filteredUsers = filteredUsers.filter((user) =>
+        user.name.toLowerCase().includes(filterValue.toLowerCase())
+      );
+    }
+
+    return filteredUsers;
+  }, [astronauts, filterValue, hasSearchFilter]);
+
+  const pages = Math.ceil(filteredItems.length / rowsPerPage);
+
   const items = useMemo(() => {
     const start = (page - 1) * rowsPerPage;
     const end = start + rowsPerPage;
 
-    return astronauts.slice(start, end);
-  }, [page, astronauts]);
+    return filteredItems.slice(start, end);
+  }, [page, filteredItems, rowsPerPage]);
+
   return (
     <>
       {astronauts ? (
-        <Table
-          className="tile col-span-3 md:col-span-3 lg:col-span-3"
-          aria-label="Astronaut table"
-          bottomContent={
-            <div className="flex w-full justify-center">
-              <Pagination
-                isCompact
-                showControls
-                showShadow
-                color="secondary"
-                page={page}
-                total={pages}
-                onChange={(page) => setPage(page)}
+        <>
+          <Table
+            className="tile col-span-3 md:col-span-3 lg:col-span-3"
+            aria-label="Astronaut table"
+            topContent={
+              <Input
+                placeholder="Search by name"
+                isClearable
+                radius="lg"
+                value={filterValue}
+                variant="bordered"
+                onClear={() => setFilterValue("")}
+                onValueChange={onSearchChange}
               />
-            </div>
-          }
-        >
-          <TableHeader className="bg-red-600">
-            <TableColumn className="text-tiny uppercase font-bold">
-              Name
-            </TableColumn>
-            <TableColumn className="text-tiny uppercase font-bold">
-              Craft
-            </TableColumn>
-          </TableHeader>
-          <TableBody items={items}>
-            {items.map((astronaut, index) => (
-              <TableRow key={index}>
-                <TableCell>{astronaut.name}</TableCell>
-                <TableCell>{astronaut.craft}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            }
+            bottomContent={
+              <div className="flex w-full justify-center">
+                <Pagination
+                  isCompact
+                  showControls
+                  showShadow
+                  color="secondary"
+                  page={page}
+                  total={pages}
+                  onChange={(page) => setPage(page)}
+                />
+              </div>
+            }
+          >
+            <TableHeader className="bg-red-600">
+              <TableColumn className="text-tiny uppercase font-bold">
+                Name
+              </TableColumn>
+              <TableColumn className="text-tiny uppercase font-bold">
+                Craft
+              </TableColumn>
+            </TableHeader>
+            <TableBody items={items}>
+              {items.map((astronaut, index) => (
+                <TableRow key={index}>
+                  <TableCell>{astronaut.name}</TableCell>
+                  <TableCell>{astronaut.craft}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </>
       ) : (
         <Card className="m-auto tile row-start-2 row-end-5 col-span-3 md:col-span-3 lg:col-span-3">
           <Spinner />
